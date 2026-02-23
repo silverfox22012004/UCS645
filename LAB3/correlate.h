52353b6e6160e973a41e6f1ed86bacb3c389ebe1
silverfox22012004 <achauhan3_be23@thapar.edu>
2026-02-23 12:36:13 +0530

    Add files via upload

---
+++ LAB3/correlate.h
@@ -0,0 +1,7 @@
+#ifndef CORRELATE_H
+#define CORRELATE_H
+
+// Function to calculate correlation between all pairs of rows
+void correlate(int ny, int nx, const float* data, float* result);
+
+#endif // CORRELATE_H
