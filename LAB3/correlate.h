f22e0f207510ca61b40f7dce5ef1a5d9278ea397
silverfox22012004 <achauhan3_be23@thapar.edu>
2026-04-22 22:12:12 +0530

    Delete LAB3 directory

--- LAB3/correlate.h
+++
@@ -1,7 +0,0 @@
-#ifndef CORRELATE_H
-#define CORRELATE_H
-
-// Function to calculate correlation between all pairs of rows
-void correlate(int ny, int nx, const float* data, float* result);
-
-#endif // CORRELATE_H
